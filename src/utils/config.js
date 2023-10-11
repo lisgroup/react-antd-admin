846dbfb17d783ced69c9847aa2cca7523e39d6ee
user66 <user66@gmail.com>
2023-10-11 17:34:31 +0800

    feat: update desc

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteName: 'AntD Admin',
-  copyright: 'Ant Design Admin  ©2020 zuiidea',
+  copyright: ' ©2023 react-antd-admin',
   logoPath: '/logo.svg',
   apiPrefix: '/api/v1',
   fixedHeader: true, // sticky primary layout header
